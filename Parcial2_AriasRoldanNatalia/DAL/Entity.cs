2a27b83865e31793b3d099fca61ad5e29ec784d9
Natalia Arias <nataliaarias523@gmail.com>
2023-04-15 10:21:32 -0500

    Creation of the entities "Entity" and "Entrance"

---
+++ Parcial2_AriasRoldanNatalia/DAL/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_AriasRoldanNatalia.DAL
+{
+    public class Entity
+    {
+        #region
+        [Key]
+        public virtual Guid id { get; set; }
+        public virtual string CreatedDate { get; set; }
+        public virtual string ModifiedDate { get; set; }
+        #endregion
+    }
+}
---
+++ Parcial2_AriasRoldanNatalia/DAL/Entrance.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_AriasRoldanNatalia.DAL
+{
+    public class Entrance : Entity
+    {
+        #region
+        [Required(ErrorMessage =" El nombre de la entrada es requerido"), 
+            MaxLength(15), 
+            Display(Name = "Entrada")]
+        public string Name { get; set; }
+        #endregion
+    }
+}
