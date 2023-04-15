8dc551fa5916a0f300e8cc266107b2b5403042cb
Natalia Arias <nataliaarias523@gmail.com>
2023-04-15 10:40:15 -0500

    create DbContext

---
+++ Parcial2_AriasRoldanNatalia/DAL/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Parcial2_AriasRoldanNatalia.DAL.Entities;
+
+namespace Parcial2_AriasRoldanNatalia.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Entrance> Entrances { get; set; }
+    }
+}
