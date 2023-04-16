01b000fa77d0bc9d1b510a90c24275cb1e6c7f7a
Natalia Arias <nataliaarias523@gmail.com>
2023-04-16 00:19:12 -0500

    update fileds isUsed in table ticket

---
+++ Parcial2_AriasRoldanNatalia/Migrations/20230415221513_UpdateFieldIsUsed.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial2_AriasRoldanNatalia.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFieldIsUsed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsUsed",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsUsed",
+                table: "Tickets",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
