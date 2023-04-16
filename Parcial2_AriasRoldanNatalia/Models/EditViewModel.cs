502ffe297ec9ee27a6986c1b0f8545a415d766a0
Natalia Arias <nataliaarias523@gmail.com>
2023-04-16 00:20:36 -0500

    Update view Edit, for select with entrace and action update

---
+++ Parcial2_AriasRoldanNatalia/Models/EditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Parcial2_AriasRoldanNatalia.DAL.Entities;
+
+namespace Parcial2_AriasRoldanNatalia.Models
+{
+    public class EditViewModel
+    {
+        public List<Entrance> Entrances { get; set;}
+        public Guid IdTicket{ get; set;}
+        public DateTime? createDateTickey { get; set;}
+
+        public string selectedEntrance { get; set; }
+    }
+}
