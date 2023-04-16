264e58a1472a9526cdeb84dd4928c876b886f705
Natalia Arias <nataliaarias523@gmail.com>
2023-04-16 00:19:36 -0500

    Modify entity Ticket

--- Parcial2_AriasRoldanNatalia/DAL/Entities/Ticket.cs
+++ Parcial2_AriasRoldanNatalia/DAL/Entities/Ticket.cs
@@ -5,7 +5,7 @@ namespace Parcial2_AriasRoldanNatalia.DAL.Entities
     public class Ticket : Entity
     {
         [Display(Name = "Usada")]
-        public bool? IsUsed { get; set; }
+        public bool IsUsed { get; set; }
 
         [Display(Name = "Fecha de uso")]
         public DateTime? UseDate { get; set; }
